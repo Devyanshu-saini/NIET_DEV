6bdc5d9b18c601f7e1fec9c4d552a4af339dfb17
Devyanshu-saini <sainid103@gmail.com>
2021-08-27 01:47:47 +0530

    update

--- JIRA APP/script.js
+++ JIRA APP/script.js
@@ -1,44 +1,89 @@
- let addbtn =document.querySelector(".add");
-let body=document.querySelector("body");
+let addBtn = document.querySelector(".add");
+let body = document.querySelector("body");
 
-addbtn.addEventListener("click",function(){
-    let div=document.createElement("div");
+let grid = document.querySelector(".grid");
 
-    div.classList.add("modal");
+let colors = ["pink", "blue", "green", "black"];
 
-    div.innerHTML=`   <div class="task-section">
-    <div class="task-inner-section " contenteditable="true"></div>
+addBtn.addEventListener("click", function () {
+  let preModal = document.querySelector(".modal");
 
+  if (preModal != null) return;
+
+  let div = document.createElement("div"); //<div></div>
+
+  div.classList.add("modal"); //<div class="modal"></div>
+
+  div.innerHTML = ` <div class="task-section">
+  <div class="task-inner-container" contenteditable="true"></div>
 </div>
-
 <div class="modal-priority-section">
-    <div class="priority-inner-container">
-        <div class="modal-priority pink"></div>
-        <div class="modal-priority blue"></div>
-        <div class="modal-priority black selected"></div>
-        <div class="modal-priority green"></div>
-    </div>
-</div>
-</div>
+  <div class="priority-inner-container">
+    <div class="modal-priority pink"></div>
+    <div class="modal-priority blue"></div>
+    <div class="modal-priority green"></div>
+    <div class="modal-priority black selected"></div>
+  </div>
 </div>`;
 
-let allModalPriority=div.querySelectorAll(".modal-priority");
+  let ticketColor = "black";
 
+  let allModalPriority = div.querySelectorAll(".modal-priority");
+  for (let i = 0; i < allModalPriority.length; i++) {
+    allModalPriority[i].addEventListener("click", function (e) {
+      for (let j = 0; j < allModalPriority.length; j++) {
+        allModalPriority[j].classList.remove("selected");
+      }
 
-for(let i=0;i<allModalPriority.length;i++)
-{
-    allModalPriority[i].addEventListener("click",function(e){
+      e.currentTarget.classList.add("selected");
 
-        for(let j=0;j<allModalPriority.length;j++)
-        {
-allModalPriority[j].classList.remove("selected")
+      ticketColor = e.currentTarget.classList[1];
+    });
+  }
+
+  let taskInnerContainer = div.querySelector(".task-inner-container");
+
+  taskInnerContainer.addEventListener("keydown", function (e) {
+    if (e.key == "Enter") {
+      let ticketDiv = document.createElement("div");
+      ticketDiv.classList.add("ticket");
+
+      ticketDiv.innerHTML = ` <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id">
+          #aeD34
+        </div>
+        <div class="actual-task">
+          ${e.currentTarget.innerText}
+        </div>`;
+
+      let ticketColorDiv = ticketDiv.querySelector(".ticket-color");
+
+      ticketColorDiv.addEventListener("click", function (e) {
+        // let colors = ["pink", "blue", "green", "black"];
+
+        let currColor = e.currentTarget.classList[1]; //green
+
+        let index = -1;
+        for (let i = 0; i < colors.length; i++) {
+          if (colors[i] == currColor) index = i;
         }
 
-        e.currentTarget.classList.add("selected")
-        
-    })
-    
-}
+        index++;
+        index = index % 4;
 
-    body.append(div);
-}); 
+        let newColor = colors[index];
+
+        ticketColorDiv.classList.remove(currColor);
+        ticketColorDiv.classList.add(newColor);
+      });
+
+      grid.append(ticketDiv);
+
+      div.remove();
+    } else if(e.key === "Escape"){
+        div.remove()
+    }
+  });
+
+  body.append(div);
+});
