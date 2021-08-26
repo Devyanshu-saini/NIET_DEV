e2ca28cd4a46d3f2b91846477d0960765ee5572a
Devyanshu-saini <sainid103@gmail.com>
2021-08-26 21:11:17 +0530

    update

--- JIRA APP/script.js
+++ JIRA APP/script.js
@@ -1,4 +1,4 @@
-/* let addbtn =document.querySelector(".add");
+ let addbtn =document.querySelector(".add");
 let body=document.querySelector("body");
 
 addbtn.addEventListener("click",function(){
@@ -6,5 +6,39 @@ addbtn.addEventListener("click",function(){
 
     div.classList.add("modal");
 
+    div.innerHTML=`   <div class="task-section">
+    <div class="task-inner-section " contenteditable="true"></div>
+
+</div>
+
+<div class="modal-priority-section">
+    <div class="priority-inner-container">
+        <div class="modal-priority pink"></div>
+        <div class="modal-priority blue"></div>
+        <div class="modal-priority black selected"></div>
+        <div class="modal-priority green"></div>
+    </div>
+</div>
+</div>
+</div>`;
+
+let allModalPriority=div.querySelectorAll(".modal-priority");
+
+
+for(let i=0;i<allModalPriority.length;i++)
+{
+    allModalPriority[i].addEventListener("click",function(e){
+
+        for(let j=0;j<allModalPriority.length;j++)
+        {
+allModalPriority[j].classList.remove("selected")
+        }
+
+        e.currentTarget.classList.add("selected")
+        
+    })
+    
+}
+
     body.append(div);
-}); */
+}); 
