fb2d529d4590677f74ac7df19da0cc057905cd5c
Devyanshu-saini <sainid103@gmail.com>
2021-09-01 15:33:58 +0530

    up

--- Excel clone/script.js
+++ Excel clone/script.js
@@ -1,13 +1,25 @@
 let rowNumberSection = document.querySelector(".row-number-section");
 
+let formulaBarSelectedCellArea = document.querySelector(".selected-cell-div");
+
+let cellSection = document.querySelector(".cell-section");
+let columnTagsSection = document.querySelector(".column-tag-section");
+
+let lastCell;
+let dataObj = {};
+
+cellSection.addEventListener("scroll", function (e) {
+  rowNumberSection.style.transform = `translateY(-${e.currentTarget.scrollTop}px)`;
+
+  columnTagsSection.style.transform = `translateX(-${e.currentTarget.scrollLeft}px)`;
+});
+
 for (let i = 1; i <= 100; i++) {
   let div = document.createElement("div");
   div.innerText = i;
   div.classList.add("row-number");
   rowNumberSection.append(div);
 }
-
-let columnTagsSection = document.querySelector(".column-tag-section");
 
 for (let i = 0; i < 26; i++) {
   let asciiCode = 65 + i;
@@ -20,34 +32,96 @@ for (let i = 0; i < 26; i++) {
   columnTagsSection.append(div);
 }
 
-
-let cellSection = document.querySelector(".cell-section")
-
+//inside this nested for loop we are creating individual cells UI + cell obj
 for (let i = 1; i <= 100; i++) {
   let rowDiv = document.createElement("div");
   rowDiv.classList.add("row");
-                       //i = 1 [A1,B1..........Z1]
-                       //i = 2 []
-                       //.
-                       //.
-                       //i = 100 [A100.........z100]
 
-  for (let j = 0; j < 26; j++) {       //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
-    // A to Z
+  for (let j = 0; j < 26; j++) {
     let asciiCode = 65 + j;
-
     let reqAlphabet = String.fromCharCode(asciiCode);
-
     let cellAddress = reqAlphabet + i;
+
+    dataObj[cellAddress] = {
+      value: undefined,
+      formula: undefined,
+      upstream: [],
+      downstream: [],
+    };
 
     let cellDiv = document.createElement("div");
 
-    cellDiv.classList.add("cell");
+    cellDiv.addEventListener("input", function (e) {
+      let currCellAddress = e.currentTarget.getAttribute("data-address");
 
+      let currCellObj = dataObj[currCellAddress];
+
+      currCellObj.value = e.currentTarget.innerText;
+      currCellObj.formula = undefined;
+
+      //1- Loop on upstream
+      //2- for each cell go to its downstream and remove ourself
+      //3- apni upstream ko empty array krdo
+
+      let currUpstream = currCellObj.upstream;
+
+      for (let k = 0; k < currUpstream.length; k++) {
+        // removeFromDownstream(parent,child)
+
+        removeFromDownstream(currUpstream[k], currCellAddress);
+      }
+
+      currCellObj.upstream = [];
+
+      console.log(currCellObj);
+    });
+
+    cellDiv.setAttribute("contentEditable", true);
+    cellDiv.classList.add("cell");
     cellDiv.setAttribute("data-address", cellAddress);
+    cellDiv.addEventListener("click", function (e) {
+      if (lastCell) {
+        lastCell.classList.remove("cell-selected");
+      }
+
+      e.currentTarget.classList.add("cell-selected");
+
+      lastCell = e.currentTarget;
+
+      let currCellAddress = e.currentTarget.getAttribute("data-address");
+
+      formulaBarSelectedCellArea.innerText = currCellAddress;
+    });
+
     rowDiv.append(cellDiv);
   }
 
-  cellSection.append(rowDiv)
+  cellSection.append(rowDiv);
+}
 
+// C1 = Formula(2*A1)
+// A1 = parent
+// C1 = child
+
+//is function kisi ki upstream se mtlb nhi hai
+//iska bs itna kaam h ki parent do and child do , aur mai parent ki downstream se child ko hta dunga
+//taki unke bichka connection khtm hojai
+//taki agr parent update ho to connection khtm hone ke baad child update na ho
+function removeFromDownstream(parentCell, childCell) {
+  //1- fetch parentCell's downstream
+
+  let parentDownstream = dataObj[parentCell].downstream;
+
+  //2- filter kro childCell ko parent ki downstream se
+
+  let filteredDownstream = []; //a1
+
+  for (let i = 0; i < parentDownstream.length; i++) { 
+    if (parentDownstream[i] != childCell) {
+      filteredDownstream.push(parentDownstream[i]);
+    }
+  }
+
+  //3- filtered upstream ko wapis save krwado dataObj me req cell me
+  dataObj[parentCell].downstream = filteredDownstream
 }
